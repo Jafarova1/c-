c9a256de2279bbffe556c58320a20b32672e8590
jafarova1 <gultajvj@code.edu.az>
2022-10-01 12:49:21 -0700

    added file

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 16, count = 0;
+            if (n > 1)
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        count++;
+                    }
+
+                }
+                if (count > 2)
+                {
+                    Console.WriteLine("murekkebdir");
+
+                }
+                else
+                {
+                    Console.WriteLine("sadedir");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("basqa reqem daxil et");
+            }
+
+        }
+    }
+}
